d5eeeb42a665176d87813603d1f9d40fd32ebc9d
김지슬 <wltmfdl123@naver.com>
2021-11-19 01:06:19 +0900

    1119 || 좋아요 라우터 연결

--- pjt-final/client/src/components/accounts/like/LikeMovieItem.vue
+++ pjt-final/client/src/components/accounts/like/LikeMovieItem.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
     like-movie-item
+    <p @click="goToMovie"></p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'LikeMovieItem',
-
+  methods: {
+    goToMovie() {
+      // this.$route.push({ name: 'MovieDetail', params: { movieId: movieId } })
+    }
+  }
 }
 </script>
 
--- pjt-final/client/src/components/accounts/like/LikePostItem.vue
+++ pjt-final/client/src/components/accounts/like/LikePostItem.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     like-post-item
+    <p @click="goToPost"></p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'LikePostItem',
-  
+  methods: {
+    goToPost() {
+      // this.$route.push({ name: 'PostDetail', params: { postId: postId } })
+    }
+  }
+
 }
 </script>
 
