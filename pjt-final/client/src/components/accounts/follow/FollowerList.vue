1ceeb50382b1972d1fc36e0522351f669f9de57c
김지슬 <wltmfdl123@naver.com>
2021-11-19 00:21:19 +0900

    1119 || 프로필의 팔로우 팔로워 세부 구현 및 버튼 생성

--- pjt-final/client/src/components/accounts/follow/FollowerList.vue
+++ pjt-final/client/src/components/accounts/follow/FollowerList.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     Follower
-    <follower-item></follower-item>
+    <follower-item 
+      @follower="reloadFollower"
+      v-for="followerItem in followers"
+      :key="followerItem.username"
+      :followerItem="followerItem"
+    ></follower-item>
   </div>
 </template>
 
@@ -12,6 +17,16 @@ export default {
   name: 'FollowerList',
   components: {
     FollowerItem
+  },
+  data() {
+    return {
+      followers: null,
+    }
+  },
+  methods: {
+    reloadFollower(followers) {
+      this.followers = followers
+    }
   }
 }
 </script>
--- pjt-final/client/src/components/accounts/follow/FollowingItem.vue
+++ pjt-final/client/src/components/accounts/follow/FollowingItem.vue
@@ -1,12 +1,40 @@
 <template>
   <div>
     following item
+    <button v-if="this.userName === this.$route.params.userName" @click="followingChangeState">??</button>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
+const SERVER_URL = process.env.VUE_APP_SERVER_URL
+
 export default {
   name: 'FollowingItem',
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    followingChangeState() {
+      this.$axios({
+        method: 'post',
+        url: `${SERVER_URL}/`,
+        headers: this.config
+      })
+        .then(() => {
+          
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  },
+  computed: {
+    ...mapState(['config', 'userName'])
+  }
 }
 </script>
 
