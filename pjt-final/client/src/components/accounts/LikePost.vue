7fc0490d91fa19d36399cf63faab28c6f66d3622
김지슬 <wltmfdl123@naver.com>
2021-11-18 20:03:18 +0900

    1118 || 프로필 버튼으로 컴포넌트 조작

--- pjt-final/client/src/components/accounts/LikePost.vue
+++ pjt-final/client/src/components/accounts/LikePost.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div>
+    like post
+  </div>
 </template>
 
 <script>
--- pjt-final/client/src/views/accounts/Profile.vue
+++ pjt-final/client/src/views/accounts/Profile.vue
@@ -1,33 +1,34 @@
 <template>
   <div>
     <h1>user</h1>
-    <!-- <button @click="">〠</button>
-    <button @click="">✎</button>
-    <button @click="">♥︎</button> -->
+    <button @click="clickFollow">〠</button> |
+    <button @click="clickMy">✎</button> |
+    <button @click="clickLike">♥︎</button>
 
-    <!-- 팔로우 팔로워 -->
     <div>
-      <following></following>
-      <follower></follower>
-    </div>
+      <!-- 팔로우 팔로워 -->
+      <div :class="{'hide': showFollow }">
+        <following></following>
+        <follower></follower>
+      </div>
 
-    <!-- 유저가 작성한 글 -->
-    <div>
-      <my-review></my-review>
-      <my-post></my-post>
-      <my-comment></my-comment>
-    </div>
+      <!-- 유저가 작성한 글 -->
+      <div :class="{'hide': showMy }">
+        <my-review></my-review>
+        <my-post></my-post>
+        <my-comment></my-comment>
+      </div>
 
-    <!-- 유저가 좋아요한 것들 -->
-    <div>
-      <like-movie></like-movie>
-      <like-post></like-post>
+      <!-- 유저가 좋아요한 것들 -->
+      <div :class="{'hide': showLike }">
+        <like-movie></like-movie>
+        <like-post></like-post>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-// import { Following } from '@/components/accounts/'
 import Following from '@/components/accounts/Following'
 import Follower from '@/components/accounts/Following'
 import MyReview from '@/components/accounts/MyReview'
@@ -47,12 +48,35 @@ export default {
     LikeMovie,
     LikePost
   },
+  data() {
+    return {
+      showFollow: false,
+      showMy: true,
+      showLike: true,
+    }
+  },
   methods: {
-
+    clickFollow() {
+      this.showFollow = false
+      this.showMy = true
+      this.showLike = true
+    },
+    clickMy() {
+      this.showFollow = true
+      this.showMy = false
+      this.showLike = true
+    },
+    clickLike() {
+      this.showFollow = true
+      this.showMy = true
+      this.showLike = false
+    }
   }
 }
 </script>
 
 <style>
-
+.hide {
+  display: none;
+}
 </style>
