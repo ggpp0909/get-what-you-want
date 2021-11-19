2d4cb22505a7716930cf31f97f1f3995e0867f62
김지슬 <wltmfdl123@naver.com>
2021-11-19 10:44:08 +0900

    1119 || 회원가입시 자동로그인에서 유저네임, 토큰 저장

--- pjt-final/client/src/store/index.js
+++ pjt-final/client/src/store/index.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
 import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
-const SERVER_URL = process.env.VUE_APP_SERVER_URL
 
 export default new Vuex.Store({
   plugins: [createPersistedState()],
   state: {
-    posts: [],
-    post: null,
     userName: null,
     token: null,
     config: null,
@@ -45,20 +41,6 @@ export default new Vuex.Store({
     },
     setToken({ commit }) {
       commit('SET_TOKEN', localStorage.getItem('jwt'))
-    },
-    // 자유게시판 전체 글 가져오기 
-    getPosts({ commit }) {
-      axios({
-        method: 'get',
-        url: `${SERVER_URL}/community/`,
-        // headers: this.state.userInfo.config
-      })
-        .then(res => {
-          commit('GET_POSTS', res.data)
-        })
-        .catch(err => {
-          console.log(err)
-        })
     },
   },
   modules: {
