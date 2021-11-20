382dbdb426395fb119ce429c7e0ca043c17e7589
장영남 <ggpp0909@naver.com>
2021-11-21 00:08:02 +0900

    nickname 에 unique=True 속성 추가, 회원정보 수정추가(닉네임, 프로필 이미지), 비밀번호 변경 추가

--- pjt-final/server/accounts/models.py
+++ pjt-final/server/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=10)
+    nickname = models.CharField(max_length=10, unique=True)
     profile_image = models.TextField(null=True)
     first_name = None
     last_name = None
